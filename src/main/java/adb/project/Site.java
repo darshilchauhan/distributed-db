26b5c169159e73e7fd270a91bcaf20c2bae5b0cd
darshil <djc621@nyu.edu>
2019-11-30 19:16:07 -0500

    site class complete

--- src/main/java/adb/project/Site.java
+++ src/main/java/adb/project/Site.java
@@ -14,6 +14,14 @@ class Site {
     Map<String, List<Integer>> readLockInfo;
     Map<String, List<Integer>> writeLockInfo;
 
+    // restrictions:
+    // commitedVals must always have all designated vars as key (it will always have
+    // some value)
+    // readLockTable must always have all designated vars as key (with value empty
+    // list if no readlocks)
+    // writeLockTable must always have all designated vars as key (with value empty
+    // string if no writelock)
+
     Site(int id) {
         totalVars = 20;
         this.id = id;
@@ -38,24 +46,46 @@ class Site {
 
         // for readlock, set will be empty if var is not readlocked
         readLockTable = new HashMap<Integer, Set<String>>();
-        for (Integer var : designatedVars) {
-            readLockTable.put(var, new HashSet<String>());
-        }
+        resetReadLockTable();
 
         // for writelocktable, string will be empty if not writelocked
         writeLockTable = new HashMap<Integer, String>();
-        for (Integer var : designatedVars) {
-            writeLockTable.put(var, "");
-        }
+        resetWriteLockTable();
 
         readLockInfo = new HashMap<String, List<Integer>>();
         writeLockInfo = new HashMap<String, List<Integer>>();
+    }
+
+    void resetReadLockTable() {
+        readLockTable.clear();
+        for (Integer var : designatedVars) {
+            readLockTable.put(var, new HashSet<String>());
+        }
+    }
+
+    void resetWriteLockTable() {
+        writeLockTable.clear();
+        for (Integer var : designatedVars) {
+            writeLockTable.put(var, "");
+        }
+    }
+
+    int readVal(int var) {
+        return commitedVals.getOrDefault(var, Integer.MIN_VALUE);
+    }
+
+    void writeVal(int var, int val) {
+        commitedVals.put(var, val);
+        if (!safeVars.contains(var))
+            safeVars.add(var);
     }
 
     // if writelocked, then return true or false depending on who holds the lock
     // if readlocked by this transaction, then return true
     // otherwise, assign lock and return true
     boolean getReadLoack(int var, String transaction) {
+        if (!safeVars.contains(var))
+            return false;
         if (writeLockTable.containsKey(var)) {
             if (writeLockTable.get(var).equals(transaction))
                 return true;
@@ -81,6 +111,8 @@ class Site {
     // writelock and return true
     // otherwise, assign writelock and return true
     boolean getWriteLock(int var, String transaction) {
+        if (!safeVars.contains(var))
+            return false;
         if (writeLockTable.containsKey(var)) {
             if (writeLockTable.get(var).equals(transaction))
                 return true;
@@ -88,17 +120,77 @@ class Site {
                 return false;
         } else if (readLockTable.containsKey(var)) {
             if (readLockTable.get(var).contains(transaction) && readLockTable.get(var).size() == 1) {
-                // update readlock to write lock
+                // upgrade readlock to write lock
                 readLockTable.get(var).clear();
                 writeLockTable.put(var, transaction);
+                // update readLockInfo and writeLockInfo
+                readLockInfo.get(transaction).remove(Integer.valueOf(var));
+                // using Integer.valueOf so it deletes by object not index
+                writeLockInfo.get(transaction).add(var);
                 return true;
             } else {
                 return false;
             }
         } else {
             writeLockTable.put(var, transaction);
+            writeLockInfo.get(transaction).add(var);
             return true;
         }
+    }
+
+    // // release a single lock from both lockTable and lockInfo
+    // void releaseReadLock(int var, String transaction) {
+    // readLockTable.get(var).remove(transaction);
+    // readLockInfo.get(transaction).remove(Integer.valueOf(var));
+    // }
+
+    // // release a single lock from both lockTable and lockInfo
+    // void releaseWriteLock(int var, String transaction) {
+    // writeLockTable.put(var, "");
+    // writeLockInfo.get(transaction).remove(Integer.valueOf(var));
+    // }
+
+    // // Note: below 2 functions don't use above 2 functions
+    // // release all locks from both lockTable and lockInfo
+    // void clearReadLocks(String transaction) {
+    // for (Integer var : readLockInfo.get(transaction)) {
+    // readLockTable.get(var).remove(transaction);
+    // }
+    // readLockInfo.get(transaction).clear();
+    // }
+
+    // // release all locks from both lockTable and lockInfo
+    // void clearWriteLocks(String transaction) {
+    // for (Integer var : writeLockInfo.get(transaction)) {
+    // writeLockTable.put(var, "");
+    // }
+    // readLockInfo.get(transaction).clear();
+    // }
+
+    // to be called during end of transaction, to release all locks and info
+    void clearTransaction(String transaction) {
+        for (Integer var : readLockInfo.get(transaction)) {
+            readLockTable.get(var).remove(transaction);
+        }
+        for (Integer var : writeLockInfo.get(transaction)) {
+            writeLockTable.put(var, "");
+        }
+        readLockInfo.remove(transaction);
+        writeLockInfo.remove(transaction);
+    }
+
+    void fail() {
+        isUp = false;
+        resetReadLockTable();
+        resetWriteLockTable();
+        readLockInfo.clear();
+        writeLockInfo.clear();
+    }
+
+    void recover(Set<Integer> newSafeVars) {
+        isUp = true;
+        safeVars.clear();
+        safeVars.addAll(newSafeVars);
     }
 
 }
