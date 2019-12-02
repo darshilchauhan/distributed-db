53807242b545a8b901bb09d3ec14996b0fb69aa4
Urvish Desai <urvish@nyu.edu>
2019-12-02 03:22:01 -0500

    remove edge test

--- src/test/java/adb/project/TestDeadlock.java
+++ src/test/java/adb/project/TestDeadlock.java
@@ -25,4 +25,16 @@ public class TestDeadlock {
         List<String> truth = new ArrayList<>();
         assertEquals(cycle, truth);
     }
+
+    @Test
+    public void testRemove() {
+        Deadlock dl = new Deadlock();
+        dl.addEdge("M1", "M2");
+        dl.addEdge("M2", "M3");
+        dl.addEdge("M3", "M1");
+        dl.removeVertex("M2");
+        List<String> cycle = dl.findCycle();
+        List<String> truth = new ArrayList<>();
+        assertEquals(cycle, truth);
+    }
 }
