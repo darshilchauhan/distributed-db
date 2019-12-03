7890c76280a88bc43cfa9ca9effc86f45862e6f0
darshil <djc621@nyu.edu>
2019-12-03 01:11:06 -0500

    uncommented testdeadlock code, fixed small error

--- src/test/java/adb/project/TestDeadlock.java
+++ src/test/java/adb/project/TestDeadlock.java
@@ -1,40 +1,40 @@
-// package adb.project;
+package adb.project;
 
-// import static org.junit.Assert.assertEquals;
-// import java.util.*;
-// import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.*;
+import org.junit.Test;
 
-// public class TestDeadlock {
+public class TestDeadlock {
 
-// @Test
-// public void testFirst() {
-// Deadlock dl = new Deadlock();
-// dl.addEdge("T1", "T2");
-// dl.addEdge("T2", "T1");
-// List<String> cycle = dl.findCycle();
-// List<String> truth = new ArrayList<>(List.of("T1", "T2"));
-// assertEquals(cycle, truth);
-// }
+    @Test
+    public void testFirst() {
+        Deadlock dl = new Deadlock();
+        dl.addEdge("T1", "T2");
+        dl.addEdge("T2", "T1");
+        List<String> cycle = dl.findCycle();
+        List<String> truth = new ArrayList<>(Arrays.asList("T1", "T2"));
+        assertEquals(cycle, truth);
+    }
 
-// @Test
-// public void testSecond() {
-// Deadlock dl = new Deadlock();
-// dl.addEdge("M1", "M2");
-// dl.addEdge("M2", "M3");
-// List<String> cycle = dl.findCycle();
-// List<String> truth = new ArrayList<>();
-// assertEquals(cycle, truth);
-// }
+    @Test
+    public void testSecond() {
+        Deadlock dl = new Deadlock();
+        dl.addEdge("M1", "M2");
+        dl.addEdge("M2", "M3");
+        List<String> cycle = dl.findCycle();
+        List<String> truth = new ArrayList<>();
+        assertEquals(cycle, truth);
+    }
 
-// @Test
-// public void testRemove() {
-// Deadlock dl = new Deadlock();
-// dl.addEdge("M1", "M2");
-// dl.addEdge("M2", "M3");
-// dl.addEdge("M3", "M1");
-// dl.removeVertex("M2");
-// List<String> cycle = dl.findCycle();
-// List<String> truth = new ArrayList<>();
-// assertEquals(cycle, truth);
-// }
-// }
+    @Test
+    public void testRemove() {
+        Deadlock dl = new Deadlock();
+        dl.addEdge("M1", "M2");
+        dl.addEdge("M2", "M3");
+        dl.addEdge("M3", "M1");
+        dl.removeVertex("M2");
+        List<String> cycle = dl.findCycle();
+        List<String> truth = new ArrayList<>();
+        assertEquals(cycle, truth);
+    }
+}
